e69d4953018c4812caef8e4124dbc288006bd0c3
Yuri Goncharov <ygpanzer@gmail.com>
2021-06-02 15:06:43 +0200

    Create FirstOne.java

---
+++ FirstOne.java
@@ -0,0 +1,17 @@
+package com.company;//Same package!!!
+
+public class FirstOne {
+
+    final int Number = 5;
+
+    protected void SomeFunction(){
+
+    }
+
+    private void SomeOtherFunction(){
+
+        SomeFunction();
+
+    }
+
+}
